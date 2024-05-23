1b77feccf0ab9b3b54d535a245acfcefa9ad1adb
Shymaa Medhat <137145389+Shymaa2611@users.noreply.github.com>
2024-05-23 13:06:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -80,8 +80,7 @@ class AudioSegmentation:
     @lru_cache(maxsize=None)
     def load_segmentation_model(self):
        pipeline = Pipeline.from_pretrained(
-        "pyannote/speaker-diarization-3.0",
-       use_auth_token="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")
+        "V-Segmentation_checkpoint\\config.yaml")
        return pipeline
     
     def audio_speech_nonspeech_detection(self,audio_url):
@@ -131,25 +130,7 @@ class SpeechToText:
  
    @lru_cache(maxsize=None)
    def load_whisper_model(self):
-    device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
-    model_id = "openai/whisper-large-v3"
-    model = AutoModelForSpeechSeq2Seq.from_pretrained(
-           model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True)
-    model.to(device)
-    processor = AutoProcessor.from_pretrained(model_id)
-    pipe = pipeline(
-     "automatic-speech-recognition",
-      model=model,
-      tokenizer=processor.tokenizer,
-      feature_extractor=processor.feature_extractor,
-      max_new_tokens=128,
-      chunk_length_s=30,
-      batch_size=16,
-      return_timestamps=True,
-      torch_dtype=torch_dtype,
-      device=device,
-    )
+    pipe=pipeline("automatic-speech-recognition",model="whisperLarge_checkpoint")
     return pipe
      
    def speech_to_text_process(self,segment,source_language:str):
@@ -198,7 +179,8 @@ class TextTranslation:
 
     @lru_cache(maxsize=None)
     def load_NLLB_model(self):
-      pipe_trans = pipeline("translation", model="facebook/nllb-200-distilled-600M")
+      #pipe_trans = pipeline("translation", model="facebook/nllb-200-distilled-600M")
+      pipe_trans=pipeline("translation",model="NLLB_checkpoint")
       return pipe_trans
     
     def split_text_into_chunks(self, text: str, chunk_size=150):
